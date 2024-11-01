dbf1b7dcbee9fea096058adfe6f5837a7fdfd5b6
Ihor Radchenko <yantar92@posteo.net>
2024-11-01 11:30:37 +0100

    imdg diff structure: Use tigher structure comparison tolerances
    The aim is to make structure comparison distinguish system with
    energies different down to 0.001eV.

--- src/IMDgroup/pymatgen/cli/imdg_diff.py
+++ src/IMDgroup/pymatgen/cli/imdg_diff.py
@@ -132,7 +132,7 @@ def diff_structures(args):
     structures = _read_structures(sorted(args.dirs), args.poscar, args.vasprun)
     groups = []
 
-    matcher = StructureMatcher(ltol=0.1, stol=0.2)
+    matcher = StructureMatcher(ltol=0.1, stol=0.1, angle_tol=3)
 
     def _add_to_groups(structure):
         """Add STRUCTURE to groups.
