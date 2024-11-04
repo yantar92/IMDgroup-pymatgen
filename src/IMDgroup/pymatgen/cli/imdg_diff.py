5cbf420481f379090e4d1e2bb2fd081f3d714905
Ihor Radchenko <yantar92@posteo.net>
2024-11-04 12:31:25 +0100

    fix

--- src/IMDgroup/pymatgen/cli/imdg_diff.py
+++ src/IMDgroup/pymatgen/cli/imdg_diff.py
@@ -279,7 +279,7 @@ def diff_incar(args):
             for key, val in group[0].items():
                 if common_incar.get(key, None) != val:
                     common_incar.pop(key, None)
-            for key, val in common_incar.items():
+            for key, val in common_incar.copy().items():
                 if group[0].get(key, None) != val:
                     common_incar.pop(key, None)
 
