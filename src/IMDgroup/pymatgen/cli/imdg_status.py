3d5ea960276ff63baa204c477bef50076eabc425
Ihor Radchenko <yantar92@posteo.net>
2024-11-04 15:29:48 +0100

    Revert "fix"
    This reverts commit c04370340fb20bcd49e104b7816196e438ee2f9d.

--- src/IMDgroup/pymatgen/cli/imdg_status.py
+++ src/IMDgroup/pymatgen/cli/imdg_status.py
@@ -49,11 +49,6 @@ def add_args(parser):
         "dir",
         help="""Directory to read (recusrively)""",
         type=str)
-    parser.add_argument(
-        "--reanalyze", "-r",
-        help="Force re-reading VASP sources",
-        action="store_true"
-    )
 
 
 def status(args):
