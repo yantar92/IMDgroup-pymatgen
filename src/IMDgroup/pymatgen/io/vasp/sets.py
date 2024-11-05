1056e5997f49b563a5f9f8fc09c32c15afeb5f36
Ihor Radchenko <yantar92@posteo.net>
2024-11-05 14:45:02 +0100

    tmp

--- src/IMDgroup/pymatgen/io/vasp/sets.py
+++ src/IMDgroup/pymatgen/io/vasp/sets.py
@@ -99,6 +99,7 @@ class IMDVaspInputSet(VaspInputSet):
             incar_updates.update(default_params)
             params = Incar.get_recipe("functional", self.functional)
             incar_updates.update(params)
+        print(incar_updates)
         return incar_updates
 
     @property
