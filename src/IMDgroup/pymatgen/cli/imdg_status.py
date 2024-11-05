3a5df8f165e938c0760307eae85b2670e2e7c61e
Ihor Radchenko <yantar92@posteo.net>
2024-11-05 09:16:02 +0100

    imdg status: Update logs

--- src/IMDgroup/pymatgen/cli/imdg_status.py
+++ src/IMDgroup/pymatgen/cli/imdg_status.py
@@ -109,7 +109,7 @@ def slurm_log_file(path):
     Return None, if the log file is not found.
     """
     files = [f for f in os.listdir(path) if os.path.isfile(f)]
-    logger.debug("Searching slurm logs across %s", files)
+    logger.debug("Searching slurm logs in %s across %s", path, files)
     matching = []
     for f in files:
         if "slurm" in f:
