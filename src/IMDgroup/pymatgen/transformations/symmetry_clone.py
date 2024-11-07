a8750d1f87c0e30c61c434de885eb4a93fd1b2ad
Ihor Radchenko <yantar92@posteo.net>
2024-11-07 12:25:24 +0100

    Implement symmetry_clone transformer

--- src/IMDgroup/pymatgen/transformations/symmetry_clone.py
+++ src/IMDgroup/pymatgen/transformations/symmetry_clone.py
@@ -3,7 +3,7 @@
 
 import logging
 from pymatgen.transformations.transformation_abc import AbstractTransformation
-from pymatgen.core import (SymmOp, Structure, Species)
+from pymatgen.core import (SymmOp, Structure, Element)
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen.util.typing import SpeciesLike
 
@@ -17,20 +17,24 @@ class SymmetryCloneTransformation(AbstractTransformation):
     def __init__(
             self,
             sym_operations: list[SymmOp] | Structure,
-            species_list: list[SpeciesLike]):
+            element_list: list[SpeciesLike]):
         """Create structure with sites cloned according to symmetry.
         Args:
          sym_operations: List of SymmOp operations to be applied or a
            reference Structure to be used to generate the operations.
          species_list: List of species to be cloned.
         """
-        self.species_set = set(map(Species, species_list))
+        self.element_set = set(map(Element, element_list))
 
         if isinstance(sym_operations, Structure):
             analyzer = SpacegroupAnalyzer(sym_operations)
             self.sym_operations = analyzer.get_symmetry_operations()
-        else:
+        elif (isinstance(sym_operations, list) and
+              isinstance(sym_operations[0], SymmOp)):
             self.sym_operations = sym_operations
+        else:
+            raise ValueError(
+                "sym_operations must be Structure of a list of SymmOp")
 
     def apply_transformation(
             self,
@@ -47,15 +51,16 @@ class SymmetryCloneTransformation(AbstractTransformation):
         Returns:
           Transformed structure.
         """
-        all_species = structure.species
-        species_to_remove = set(all_species) - self.species_set
+        all_elements = map(Element, structure.species)
+        elements_to_remove = set(all_elements) - self.element_set
         filled_structure = structure.copy()
         clean_structure = structure.copy()
-        clean_structure.remove_species(list(species_to_remove))
+        clean_structure.remove_species(list(elements_to_remove))
         for op in self.sym_operations:
             tmp_structure = clean_structure.copy()
             tmp_structure.apply_operation(op)
             for site in tmp_structure:
-                filled_structure.append(site.species, site.coords)
+                filled_structure.append(
+                    site.species, site.coords, properties=site.properties)
         filled_structure = filled_structure.merge_sites(mode='average')
         return filled_structure
