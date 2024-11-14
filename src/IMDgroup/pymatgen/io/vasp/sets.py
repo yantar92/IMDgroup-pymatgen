ff7d536630cbc32643210a3328a85fde746852d4
Ihor Radchenko <yantar92@posteo.net>
2024-11-14 10:36:03 +0100

    Fix linter warning

--- src/IMDgroup/pymatgen/io/vasp/sets.py
+++ src/IMDgroup/pymatgen/io/vasp/sets.py
@@ -142,8 +142,8 @@ class IMDVaspInputSet(VaspInputSet):
                     "\nI hope that you know what you are doing.",
                     BadInputSetWarning,
                 )
-            elif incar['ENCUT'] > 500.0 and\
-                 incar['ISIF'] == Incar.ISIF_FIX_SHAPE_VOL:
+            elif (incar['ENCUT'] > 500.0 and
+                  incar['ISIF'] == Incar.ISIF_FIX_SHAPE_VOL):
                 warnings.warn(
                     "ENCUT parameter is too high for position relaxation."
                     f" ({incar['ENCUT']} > 500eV)"
