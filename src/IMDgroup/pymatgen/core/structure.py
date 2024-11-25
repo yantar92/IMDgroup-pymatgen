0402ac7ef0ea348ed223b6398768a30e6faf0a64
Ihor Radchenko <yantar92@posteo.net>
2024-11-25 11:42:50 +0100

    merge_structures: New parameter tolerance

--- src/IMDgroup/pymatgen/core/structure.py
+++ src/IMDgroup/pymatgen/core/structure.py
@@ -3,9 +3,13 @@
 from pymatgen.core import Structure
 
 
-def merge_structures(structs: list[Structure]) -> Structure:
+def merge_structures(
+        structs: list[Structure],
+        tol: float = 0.01,
+        ) -> Structure:
     """Merge all STRUCTS into a single Structure.
     Return structure.
+    tol is tolerance passed to Structure.merge_sites method.
     """
     assert len(structs) > 0
     for struct in structs[1:]:
@@ -18,5 +22,5 @@ def merge_structures(structs: list[Structure]) -> Structure:
                 site.species,
                 site.frac_coords,
                 properties=site.properties)
-    merged.merge_sites(mode='average')
+    merged.merge_sites(mode='average', tol=tol)
     return merged
