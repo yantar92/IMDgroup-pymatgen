0335edade66fbb57e9687980c9e9bc139b52c32d
Ihor Radchenko <yantar92@posteo.net>
2024-11-26 08:44:31 +0100

    merge_structures: Add logging

--- src/IMDgroup/pymatgen/core/structure.py
+++ src/IMDgroup/pymatgen/core/structure.py
@@ -1,6 +1,9 @@
 """Extension for pymatgen.core.structure
 """
+import logging
 from pymatgen.core import Structure
+
+logger = logging.getLogger(__name__)
 
 
 def merge_structures(
@@ -11,6 +14,7 @@ def merge_structures(
     Return structure.
     tol is tolerance passed to Structure.merge_sites method.
     """
+    logger.debug("Merging %d structures...", len(structs))
     assert len(structs) > 0
     for struct in structs[1:]:
         assert struct.lattice == structs[0].lattice
@@ -22,5 +26,9 @@ def merge_structures(
                 site.species,
                 site.frac_coords,
                 properties=site.properties)
+    sites_before = len(merged)
     merged.merge_sites(mode='average', tol=tol)
+    logger.debug(
+        "Merging %d structures... done (%d -> %d atoms)",
+        len(structs), sites_before, len(merged))
     return merged
