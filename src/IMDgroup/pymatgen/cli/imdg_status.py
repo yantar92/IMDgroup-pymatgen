c22e473e09e6e5f14d93788b11fc1680e5073145
Ihor Radchenko <yantar92@posteo.net>
2024-12-11 08:43:11 +0100

    imdg status: Do not use warning color for update time

--- src/IMDgroup/pymatgen/cli/imdg_status.py
+++ src/IMDgroup/pymatgen/cli/imdg_status.py
@@ -420,7 +420,7 @@ def status(args):
         mtime = vasp_output_time(wdir)
         delta = mtime - datetime.datetime.now().timestamp()
         print(
-            colored(f"[{print_seconds(delta): >15}]", color="yellow"),
+            colored(f"[{print_seconds(delta): >15}]", color="grey"),
             colored(f"{wdir.replace("./", "")}:", attrs=['bold']),
             run_prefix + run_status + progress + warning_list)
 
