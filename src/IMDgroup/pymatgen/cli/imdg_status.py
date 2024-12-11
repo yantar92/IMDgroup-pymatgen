cde333f617ab1381380b7251ac8720ed7c6b6acf
Ihor Radchenko <yantar92@posteo.net>
2024-12-11 12:59:15 +0100

    imdg status: Display suspiciously large relaxation steps

--- src/IMDgroup/pymatgen/cli/imdg_status.py
+++ src/IMDgroup/pymatgen/cli/imdg_status.py
@@ -27,6 +27,13 @@ VASP_WARNINGS = {
         "slurmstepd: error.+",
         "prterun noticed.+",
     ],
+    "relaxation_step" : [
+        # Very large atom displacement during relaxation
+        # >=0.06
+        "^.*dis = [1-9].*$",
+        "^.*dis = 0.[1-9].*$",
+        "^.*dis = 0.0[6-9].*$",
+    ],
     "electron_convergance": [
         "The electronic self-consistency was not achieved in the given.+"
     ],
