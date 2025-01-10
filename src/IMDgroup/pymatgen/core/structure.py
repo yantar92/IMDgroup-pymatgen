5f27ec1a515eace22dc5270bd61c888b65a37ccc
Ihor Radchenko <yantar92@posteo.net>
2025-01-10 08:49:03 +0100

    merge_structures: Remove workaround for pymatgen bug#4197
    * src/IMDgroup/pymatgen/core/structure.py (merge_structures):
    Remove the workaround.  We now depend on pymatgen version where this
    bug has been fixed.

--- src/IMDgroup/pymatgen/core/structure.py
+++ src/IMDgroup/pymatgen/core/structure.py
@@ -20,29 +20,6 @@ def merge_structures(
     for struct in structs[1:]:
         assert struct.lattice == structs[0].lattice
 
-    prop_values = {}
-
-    def store_array_props(struct):
-        """Convert array properties to strings.
-        This is to work around pymatgen bug#4197 where merging array
-        properties fails.
-        """
-        for site in struct:
-            for name, val in site.properties.items():
-                if isinstance(val, np.ndarray):
-                    site.properties[name] = str(val)
-                    prop_values[str(val)] = val
-
-    def restore_array_props(struct):
-        """Convert back strings to arrays.
-        This is to work around pymatgen bug#4197 where merging array
-        properties fails.
-        """
-        for site in struct:
-            for name, val in site.properties.items():
-                if isinstance(val, str) and val in prop_values:
-                    site.properties[name] = prop_values[val]
-
     merged = structs[0].copy()
 
     sites_before = sum(len(s) for s in structs)
@@ -54,9 +31,7 @@ def merge_structures(
                 site.species,
                 site.frac_coords,
                 properties=site.properties)
-        store_array_props(merged)
         merged.merge_sites(mode='average', tol=tol)
-    restore_array_props(merged)
     logger.debug(
         "Merged %d structures (%d -> %d atoms)",
         len(structs), sites_before, len(merged))
