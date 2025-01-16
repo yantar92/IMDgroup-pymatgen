799f015acb7b58926bc1af3289ce0796585370c9
Ihor Radchenko <yantar92@posteo.net>
2025-01-16 11:59:23 +0100

    Revert "neb: Improve multithreading (try 2)"
    This reverts commit d8bf76dc32b82059605743e82222a6beb77bad88.

--- src/IMDgroup/pymatgen/diffusion/neb.py
+++ src/IMDgroup/pymatgen/diffusion/neb.py
@@ -96,20 +96,8 @@ class _StructFilter():
         if dist > self.cutoff or dist < self.tol:
             return False
         if self.discard_equivalent:
-            if self.multithread:
-                with Pool() as pool:
-                    distances = pool.starmap(
-                        structure_distance,
-                        [(clone, rej) for rej in self.rejected],
-                        chunksize=20
-                    )
-            else:
-                distances = None
-            for idx, rej in enumerate(self.rejected):
-                if distances is None:
-                    dist = structure_distance(clone, rej)
-                else:
-                    dist = distances[idx]
+            for rej in self.rejected:
+                dist = structure_distance(clone, rej)
                 if dist < self.tol:
                     return False
                 if self.multithread:
