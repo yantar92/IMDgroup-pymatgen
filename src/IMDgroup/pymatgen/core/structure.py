9d4148720135c96867994ae209b415db0a382305
Ihor Radchenko <yantar92@posteo.net>
2025-02-06 09:02:19 +0100

    structure_interpolate2: Set back CENTER=0.5 by default

--- src/IMDgroup/pymatgen/core/structure.py
+++ src/IMDgroup/pymatgen/core/structure.py
@@ -88,13 +88,13 @@ def structure_interpolate2(
         structure1: Structure, structure2: Structure,
         nimages: int = 10,
         frac_tol: float = 0.5,
-        center: bool | float = 1.0,
+        center: bool | float = 0.5,
         **kwargs) -> list[Structure]:
     """Like Structure.interpolate, but make sure that images are valid.
     Valid means that no atoms in the images are very close
     (structure_is_valid2), no closer than FRAC_TOL*sum of specie radiuses.
 
-    A new parameter CENTER (default: 1.0Å) will adjust STRUCTURE2 to
+    A new parameter CENTER (default: 0.5Å) will adjust STRUCTURE2 to
     match STRUCTURE1 geometric center of mass before interpolation.
     CENTER may either be a boolean (True to adjust structure centers)
     or a float to adjust structure centers only when the distance
