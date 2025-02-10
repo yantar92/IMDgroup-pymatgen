get_neb_pairs: Fix numerical error in float comparison
* src/IMDgroup/pymatgen/diffusion/neb.py (get_neb_pairs):
Assign a slightly larger value for energy threshold to avoid floating
point errors influencing low-energy structure selection.

src/IMDgroup/pymatgen/diffusion/neb.py
@@ -521,7 +521,7 @@ def get_neb_pairs(
         logger.debug("Energy %d: %f", idx, energy)
         energies.append(energy)
     # Only take the lowest-energy structure
-    energy_threshold = sorted(np.unique(energies))[0]
+    energy_threshold = sorted(np.unique(energies))[0] + 1E-9
     logger.info("Energy theshold: %f", energy_threshold)
 
     low_en_idxs = [