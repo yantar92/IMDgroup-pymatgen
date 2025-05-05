1335c2e8db605396548f03c5f95ff6cfec88aa1a
Ihor Radchenko <yantar92@posteo.net>
2025-05-05 13:09:19 +0200

    fix

--- src/IMDgroup/pymatgen/cli/imdg_analyze.py
+++ src/IMDgroup/pymatgen/cli/imdg_analyze.py
@@ -221,7 +221,7 @@ def read_vaspruns(rootdir, path_filter=None):
     """
     drone = IMDGVaspToComputedEnrgyDrone(
         inc_structure=True,
-        data=["filename", "initial_structure", "incar", 'converged', 'logs'])
+        data=["filename", "initial_structure", "incar", 'converged'])
 
     if os.access(rootdir, os.W_OK):
         dump_file = os.path.join(rootdir, SAVE_FILE)
