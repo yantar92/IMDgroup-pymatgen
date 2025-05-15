7543a7481bb689fc2ab2d5a234e2883586ca0aaf
Ihor Radchenko <yantar92@posteo.net>
2025-05-15 09:49:47 +0200

    imdg status: Do not print progress position in status logs

--- src/IMDgroup/pymatgen/cli/imdg_status.py
+++ src/IMDgroup/pymatgen/cli/imdg_status.py
@@ -266,7 +266,7 @@ def status(args):
         for wdir in paths_no_output:
             print("  ", wdir)
     all_warn_names_present = set()
-    for wdir in alive_it(paths):
+    for wdir in alive_it(paths, enrich_print=False):
         vaspdir = vaspdirs.get(wdir)
         nebp = vaspdir.nebp
         converged = vaspdir.converged
