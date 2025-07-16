b195abc959207885a4b0118834275596f949f74c
Ihor Radchenko <yantar92@posteo.net>
2025-07-16 09:56:17 +0200

    test2

--- src/IMDgroup/pymatgen/cli/imdg_visualize.py
+++ src/IMDgroup/pymatgen/cli/imdg_visualize.py
@@ -410,7 +410,7 @@ def _atat_plot_calculated_energies(
             for index, concentration, energy in zip(
                     df['index'], df['concentration'], df['energy']):
                 orig_energy = fit.loc[fit['index'] == index, 'energy']
-                print(orig_energy)
+                print(fit.loc[fit['index'] == index])
                 if np.isclose(energy, orig_energy, atol=df.threshold):
                     continue
                 if energy < orig_energy:
