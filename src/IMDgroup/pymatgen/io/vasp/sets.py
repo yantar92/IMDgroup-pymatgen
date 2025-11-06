b8b93a66da33c9843fbbaff3554f198a7c3ae01e
Ihor Radchenko <yantar92@posteo.net>
2025-11-06 13:39:40 +0100

    IMDDerivedInputSet: Fix extra INCAR name to transfer
    Should not be something like ../INCAR.1

--- src/IMDgroup/pymatgen/io/vasp/sets.py
+++ src/IMDgroup/pymatgen/io/vasp/sets.py
@@ -460,7 +460,7 @@ class IMDDerivedInputSet(IMDVaspInputSet):
             logger.debug(
                 "Reading previous VASP output from %s", self.directory)
             for extra_incar in Path(self.directory).glob('INCAR.[0-9]*'):
-                self.files_to_transfer[str(extra_incar)] = extra_incar
+                self.files_to_transfer[str(extra_incar.name)] = extra_incar
             self.override_from_prev_calc(prev_calc_dir=self.directory)
         except ValueError as exc:
             if os.path.isfile(os.path.join(self.directory, "CONTCAR")):
