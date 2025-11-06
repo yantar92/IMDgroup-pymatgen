97c9fd647054b1c3257f3553be82493439c16a56
Ihor Radchenko <yantar92@posteo.net>
2025-11-06 13:11:19 +0100

    IMDDerivedInputSet: Fix key type for self.files_to_transfer

--- src/IMDgroup/pymatgen/io/vasp/sets.py
+++ src/IMDgroup/pymatgen/io/vasp/sets.py
@@ -460,7 +460,7 @@ class IMDDerivedInputSet(IMDVaspInputSet):
             logger.debug(
                 "Reading previous VASP output from %s", self.directory)
             for extra_incar in Path(self.directory).glob('INCAR.[0-9]*'):
-                self.files_to_transfer[extra_incar] =\
+                self.files_to_transfer[str(extra_incar)] =\
                     Incar.from_file(extra_incar)
             self.override_from_prev_calc(prev_calc_dir=self.directory)
         except ValueError as exc:
