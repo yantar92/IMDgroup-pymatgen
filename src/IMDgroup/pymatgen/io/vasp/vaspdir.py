fd5ab1b9daf103a99142277f945d9fd548532563
Ihor Radchenko <yantar92@posteo.net>
2026-02-04 10:38:51 +0100

    Increase LMDB map size

--- src/IMDgroup/pymatgen/io/vasp/vaspdir.py
+++ src/IMDgroup/pymatgen/io/vasp/vaspdir.py
@@ -147,7 +147,7 @@ class IMDGVaspDir(collections.abc.Mapping, MSONable):
         try:
             cls._lmdb_env = lmdb.open(
                 str(db_path),
-                map_size=2**30,  # 1GB
+                map_size=2**33,  # 8GB
                 max_dbs=1,
                 lock=True,
                 subdir=False,
